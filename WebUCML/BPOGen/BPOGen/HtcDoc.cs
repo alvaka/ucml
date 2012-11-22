3fd719182fdcdbd1d4660ea8c363f36afa2762d6
alvaka <alvaka@163.com>
2012-11-22 22:00:12 +0800

    add HtcDoc
    add HtcDoc class to manage HTC file

---
+++ WebUCML/BPOGen/BPOGen/HtcDoc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UCML.IDE.WebUCML
+{
+    public class HtcDoc
+    {
+        public string Name;
+        public StringBuilder Definition;
+        public StringBuilder Statement;
+        public List<JsFunction> FuncList;
+        public HtcDoc(string name)
+        {
+            this.Name = name;
+            this.Definition = new StringBuilder();
+            this.Statement = new StringBuilder();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.Definition.ToString());
+            sb.AppendLine("<script language=\"jscript\">");
+            sb.AppendLine(this.Statement.ToString());
+            foreach (JsFunction fun in this.FuncList)
+            {
+                sb.AppendLine();
+            }
+            sb.AppendLine("</script>");
+            
+            return base.ToString();
+        }
+    }
+}
